4eba566bba1e41267db9750990b180107b2eafb7
Piotr <sztajglaP@interia.eu>
2023-05-08 09:24:55 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import CardInfo from "./components/CardInfo";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Menu from "./components/Menu";
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,70 +1,72 @@
 import useFetch from '../useFetch';
 import './main.css';
-
-
-
-// const data = [
-//     {
-//         'name': 'todo',
-//         'tasks': [
-//             {
-//                 'name': 'Piotr',
-//                 'title': 'Odebrac paczkę',
-//                 'id': 0
-//             }
-//         ],
-//     },
-//     {
-//         'name': 'workinprogress',
-//         'tasks': [
-//             {
-//                 'name': 'Piotr',
-//                 'title': 'asd',
-//                 'id': 0
-//             }
-//         ],
-//     },
-//     {
-//         'name': 'done',
-//         'tasks': [
-//             {
-//                 'name': 'Piotr',
-//                 'title': 'Odebrac paczkę',
-//                 'id': 0
-//             }
-//         ]
-//     },
-// ]
-
-// const createTaskCards = (area) => {
-//     return (
-//         area.tasks.map(task => (
-//             <div className="task-card" key={task.id}>
-//                 <div className="card-user-info">
-//                     <div className="user-photo"></div>
-//                     <div className="user-name">{task.name}</div>
-//                 </div>
-//                 <div className="task-short-description">
-//                     <p className='task-title'>{task.title}</p>
-//                 </div>
-//             </div>
-//         ))
-//     );
-// }
+import './cardInfo.css';
+import { useState } from 'react';
 
 const Main = () => {
     const data = useFetch('http://localhost:8000/areas');
+    const [popup, setPopup] = useState('inactive');
+    const [task, setTask] = useState(null);
+
+    const createTaskCards = (task) => {
+        return (
+            <div className="task-card" key={task.id} onClick={() => {
+                setTask(task);
+                setPopup('active')
+            }}>
+                <div className="card-info-box">
+                    <div className='card-user-info'>
+                        <div className="user-photo"></div>
+                        <div className="user-name">{task.name}</div>
+                    </div>
+                    <div className="date">{task.date}</div>
+                </div>
+                <div className="task-short-description">
+                    <p className='task-title'>{task.title}</p>
+                </div>  
+            </div> 
+        );
+    }
 
     return (
         <div className="main">
-            <div className="main-container">
+
+            {
+                <div className={`card-info-popup ${popup}`}>
+                    <div className="popup-content">
+                        <button className="popup-close-btn" onClick={() => setPopup('inactive')}>X</button>
+                        <div className="popup-info-box">
+                            <div className="popup-user-info">
+                                <div className="popup-user-image"></div>
+                                {task && <div className="popup-user-name">{task.name}</div>}
+                            </div>
+                            {task && <div className="popup-date">{task.date}</div>}
+                        </div>
+                        {task && <p className="popup-title">{task.title}</p>}
+                        <hr />
+                        {task && <ul className="popup-comments">
+                            {task.comments.map(comment => (
+                                <li key={comment.id}>
+                                    {comment.date}
+                                    <span>{comment.text}</span>
+                                </li> 
+                            ))}          
+                        </ul>}
+                        <p>Dodaj komentarz</p>
+                        <input type="text" className='comment-input'/>
+                        <button className="popup-add-comment">Dodaj komentarz</button>
+                    </div>
+                </div>
+            }
+
+            <div className="main-container">    
 
                 <div className="task-area-box">
                     <h2 className="task-area-title">To Do</h2>
                     <div className="task-area">
 
                         {
-                            data && data.map(el => (console.log(el)))
+                            data && data[0].tasks.map(task => createTaskCards(task))
                         }
 
                     </div>
@@ -74,9 +76,9 @@ const Main = () => {
                     <h2 className="task-area-title">Work in progress</h2>
                     <div className="task-area">
                         
-                        {/* {taskAreas.map(area => (
-                            area.name === 'workinprogress' && createTaskCards(area)
-                        ))} */}
+                        {
+                            data && data[1].tasks.map(task => createTaskCards(task))
+                        }
 
                     </div>
                 </div>
@@ -85,9 +87,9 @@ const Main = () => {
                     <h2 className="task-area-title">Done</h2>
                     <div className="task-area">
                         
-                        {/* {taskAreas.map(area => (
-                            area.name === 'done' && createTaskCards(area)
-                        ))} */}
+                        {
+                            data && data[2].tasks.map(task => createTaskCards(task))
+                        }
 
                     </div>
                 </div>
